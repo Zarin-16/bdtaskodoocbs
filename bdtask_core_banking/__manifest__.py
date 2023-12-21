55cae31280a36db8685d5880788f32095dcae756
Zarin Tasnim <tasnimzarin427@gmail.com>
2023-12-21 16:43:19 +0600

    [ADD] banner

--- bdtask_core_banking/__manifest__.py
+++ bdtask_core_banking/__manifest__.py
@@ -3,8 +3,12 @@
 {
     'name': 'Core Banking System',
     'version': '1.0',
-    'summary': 'Core Banking 2',
-    'description': 'Core Banking 3',
+    'summary': 'Core Banking',
+    'description': 'A Core Banking System (CBS) serves as the central nerve system of a bank, \
+                    providing a comprehensive and integrated platform for managing various banking functions.\
+                    It is designed to streamline and automate core banking operations, ensuring efficiency, accuracy, \
+                    and seamless customer service. The system acts as the backbone, \
+                    connecting various banking modules and enabling smooth interaction between different banking components.',
     'category': 'Banking addons',
     'author': 'Bdtask Limited(A Leading Software Company)',
     'company': 'Bdtask Limited',
@@ -13,6 +17,7 @@
     'website': 'https://www.bdtask.com/',
     'data': [
     'security/ir.model.access.csv',
+    'images': ['static/description/banner.gif'],
 
     # 'data/demo_data.xml',
 
@@ -33,6 +38,7 @@
 
     'views/cbs_menus.xml',
     ],
+    
     
     'license': 'LGPL-3',
     'installable': True,
